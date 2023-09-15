1c380e974437bab98a6cea2cc727ce563a3c4ccb
User <User@B26>
2023-09-15 11:17:32 +0800

    Add project files.

---
+++ App2/App2/Model/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.Model
+{
+    internal class TaskModel
+    {
+        public string Subjectcode { get; set; }
+        public string SubjectTitle { get; set; }
+
+        public int Unit { get; set; }
+    }
+}
---
+++ App2/App2/View/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App2.ViewModel;
+
+namespace App2.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = new TaskViewModel();
+        }
+    }
+}
